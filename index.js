8f5d32360ccf9089dedf757c9e32cd882d7c2ba8
Sachin Jha <139990682+sachinjha03@users.noreply.github.com>
2025-03-09 22:17:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const app = express();
-const cors = require('cors')
+// const cors = require('cors')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 // app.use(cors())
-app.use(
-  cors({
-    origin: "https://wevaccinate.netlify.app", // Replace with your actual frontend domains
-    credentials: true, // Allow cookies & authentication headers
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://wevaccinate.netlify.app", // Replace with your actual frontend domains
+//     credentials: true, // Allow cookies & authentication headers
+//   })
+// );
+
+app.use((req,res,next) => {
+  res.setHeader("Access-Control-Allow-Origin" , "https://wevaccinate.netlify.app/");
+  res.header(
+    "Access-Control-Allow-Headers" , 
+    "Origin , X-Requested-With, Content-Type , Accept"
+    );
+  next();
+})
 
 
 
