8cf4ecfff65eecf536253cc1bd8bf1b7e70a7c8c
Sachin Jha <139990682+sachinjha03@users.noreply.github.com>
2025-03-13 10:55:32 +0530

    updating index file

--- index.js
+++ index.js
@@ -1,30 +1,11 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const PORT = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-// app.use(cors())
-
-
-app.use(
-  cors({
-    origin: "https://we-vaccinate.netlify.app/", // Replace with your actual frontend domains
-    credentials: true, // Allow cookies & authentication headers
-  })
-);
-
-
-
-// app.use((req,res,next) => {
-//   res.setHeader("Access-Control-Allow-Origin" , "https://wevaccinate.netlify.app/");
-//   res.header(
-//     "Access-Control-Allow-Headers" , 
-//     "Origin , X-Requested-With, Content-Type , Accept"
-//     );
-//   next();
-// })
-
+app.use(cors({credentials: true}));
 
 
 require("./db/connection")
@@ -34,7 +15,7 @@ app.use("/api" , require("./routes/Query"))
 app.use("/api" , require("./routes/User"))
 app.use("/api" , require("./routes/Appointment"))
 
-app.listen(8000 , () => {
-    console.log("Hello from backend");
+app.listen(PORT , () => {
+    console.log("Hello from backend , Listening at " + PORT);
     
 })
